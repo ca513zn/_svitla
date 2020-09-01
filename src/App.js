9e2745b0e183e51bc2c71c7feab2a8649708c0f5
czuniga <czuniga@tibco-support.com>
2020-08-31 22:45:00 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = React.useState([]);
+  const [todo, setTodo] = React.useState("");
+
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const handleRemove = (todoToDelete) => {
+    setTodos(todos.filter((item) => item != todoToDelete));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodos(todos.concat(todo));
+    setTodo("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">Welcome to the App</div>
+      <div className="todo">
+        <div className="todo__input">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={todo}
+              placeholder="Type here to add item"
+              onChange={(e) => handleChange(e)}
+            />
+          </form>
+        </div>
+        <div className="todo__list">
+          <ul>
+            {(todos.length > 0 || todo.length > 0) && (
+              <li key="list-header">
+                <div>Task</div>
+                <div>Delete</div>
+              </li>
+            )}
+            {todos.length === 0 && todo.length === 0 ? (
+              <div>NOTHING TO DO!</div>
+            ) : (
+              todos.map((todo, i) => (
+                <li key={"todo-" + i}>
+                  <div>{todo}</div>
+                  <div
+                    style={{
+                      marginLeft: "16px",
+                    }}
+                  >
+                    <input
+                      type="checkbox"
+                      onChange={() => handleRemove(todo)}
+                    />
+                  </div>
+                </li>
+              ))
+            )}
+            {todo.length > 0 ? (
+              <li key={"new-todo"}>
+                <div>{todo}</div>
+              </li>
+            ) : null}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
